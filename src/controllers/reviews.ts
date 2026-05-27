499ecce1937ebb580133e846b9731a972ef6895c
lesley <sprite_lksln31@yahoo.com>
2026-05-27 13:09:01 -1000

    git commit -m "Implementing CRUD Operations on the Reviews Resource"

--- src/controllers/reviews.ts
+++ src/controllers/reviews.ts
@@ -1,3 +1,26 @@
+export const getReviews = async (req: Request, res: Response) => {
+	try {
+		const reviews = await Review.find().populate('book');
+		res.status(200).send(reviews);
+	} catch (_err) {
+		res.status(500).send({ message: 'Server error' });
+	}
+};
 import type { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Review from '../models/review.js';
+
+export const createReview = async (req: Request, res: Response) => {
+	const { text, rating, bookId } = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(String(bookId))) {
+		return res.status(400).send({ message: 'Invalid ID' });
+	}
+
+	try {
+		const review = await Review.create({ text, rating, book: String(bookId) });
+		res.status(201).send(review);
+	} catch (_err) {
+		res.status(500).send({ message: 'Server error' });
+	}
+};
--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import { createReview, getReviews } from '../controllers/reviews.js';
 
 const router = Router();
+
+
+router.get('/', getReviews);
+router.post('/', createReview);
 
 export default router;
