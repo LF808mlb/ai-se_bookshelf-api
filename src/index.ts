5f4ae9fc78e8a0c7da1f98bc1c710db93c80904d
lesley <sprite_lksln31@yahoo.com>
2026-05-27 09:25:34 -1000

    git commit -m "connect to mongoDB"

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 3000;
 
+mongoose.connect('mongodb://127.0.0.1:27017/bookshelfdb')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Connection error', err));
+  
 app.use(express.json());
 
 app.listen(PORT, () => {
